8975fea43d3444b6087e06b73aae03d143d0a705
Timo Wilhelm <mail@timowilhelm.com>
2026-05-11 07:59:28 +0200

    update lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,7 @@
 export default {
-	'*.{ts,tsx}': (stagedFiles) => [
-		'bun run typecheck',
-		`prettier --list-different ${stagedFiles.join(' ')}`,
-		`eslint ${stagedFiles.join(' ')}`,
-	],
+	'*.{ts,tsx}': () => ['bun run typecheck'],
+	'*.{js,mjs,cjs,jsx,ts,tsx}': (stagedFiles) => [`prettier --list-different ${stagedFiles.join(' ')}`, `eslint ${stagedFiles.join(' ')}`],
+	'*.{json,jsonc,css,html}': (stagedFiles) => `prettier --list-different ${stagedFiles.join(' ')}`,
 	'*.md': (stagedFiles) => `prettier --list-different ${stagedFiles.join(' ')}`,
 	'landing/**/*.{astro,ts,tsx,js,json,css}': () => ['bun run --cwd landing format', 'bun run --cwd landing lint'],
 };
